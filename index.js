d51c647177ea4918318b8a0a0c4a00a483928cb9
时刻记得我 <109955134+Neohero521@users.noreply.github.com>
2026-03-11 10:50:59 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // 严格遵循官方模板导入规范，路径完全对齐原版本
 import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
 import { saveSettingsDebounced } from "../../../../script.js";
-const extensionName = "Always_remember_me";
+const extensionName = "Verification";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 // 预设章节拆分正则列表（覆盖全场景，含括号序号格式）
 const presetChapterRegexList = [
